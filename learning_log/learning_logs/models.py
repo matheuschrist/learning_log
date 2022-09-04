6de8473c4da17fa6f56101e798bb93b645b2927a
Matheus <Matheusbrau12@gmail.com>
2022-09-04 17:19:26 -0300

    Enviando atualizações

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -13,7 +13,7 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Algo específico aprendido sobre um assunto.""" 
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True) 
     class Meta:
